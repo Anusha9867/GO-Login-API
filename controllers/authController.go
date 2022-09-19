36868ad51b46db2481f8901be2407a2ccd236506
Anusha R <anusha.r@optisolbusiness.com>
2022-09-19 08:41:24 +0530

    Code changes

--- controllers/authController.go
+++ controllers/authController.go
@@ -1,8 +1,6 @@
 package controllers
 
 import (
-	// "fmt"
-
 	"login/database"
 	"login/models"
 	"strconv"
--- models/password.go
+++ models/password.go
@@ -1,6 +1,9 @@
 package models
 
+import "gorm.io/gorm"
+
 type Password struct {
+	gorm.Model
 	UserId   uint   `json:"user_id"`
 	Password []byte `json:"-"`
 }
